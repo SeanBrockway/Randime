18f1355847cb0e0dce5ff33fe1ddc01ed99c052a
Sean Brockway <seanbrockway97@gmail.com>
2020-05-21 17:22:45 +0200

    Removed zip, added material ui and fonts file

--- randime/src/App.js
+++ randime/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import './Components/css/bootstrap.min.css';
 import './Components/css/style.css';
+import './Components/css/fonts.css';
 import SectionHeader from './Components/SectionHeader.js'
 
 function App() {
